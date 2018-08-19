77e5fbbc81c9048784af44e49454e7c93b346ac3
cismous <cismous@gmail.com>
2018-08-20 00:56:34 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Usage
+
+## Installation
+
+`npm install`
+
+## Config
+
+add file `config.local.js` to project root directory.
+
+add content `{ module.exports = { token: '', chatID: '' } }` to `config.local.js` file.
+
+Get token from https://core.telegram.org/bots/api
+
+Get chatID from telegram channel id
+
+## Run
+
+`npm run start`
--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const story = require('./src/story');
-const Bot = require('./src/bot');
 const store = require('./src/store');
 const log = require('./src/log');
 const config = require('./src/config')();
 
-function tokenError() {
-  log.err('Telegram bot token is needed!');
-}
-
-if (!config.token || !config.chatID) return tokenError();
+if (!config.token) return log.err('Telegram bot token is needed!');
 if (!config.chatID) return log.err('Telegram chat id is needed!');
-const bot = Bot(config.token);
-if (!config.domain || !config.bookUrl) return log.err('book config error!');
+const bot = require('./src/bot')(config.token);
 
+if (!config.domain || !config.bookUrl) return log.err('book config error!');
 story({ domain: config.domain, bookUrl: config.bookUrl }).then(result => {
-  if (result === null) return
+  if (result === null) return;
 
   const { title, content } = result;
   if (store.has(title)) return log.std(`Chapter: ${title} already existed!`);
